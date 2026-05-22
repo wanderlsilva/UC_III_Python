a9c96db724a4cfa9762196991553cb0d181ab18c
Wander Luiz da Silva Flores <wanderluiz2312@gmail.com>
2026-05-22 19:26:12 -0400

    update advinha

--- Revisao/advinha6.py
+++ Revisao/advinha6.py
@@ -1,16 +1,20 @@
+import random
 print("-"*10, ' ADVINHAR NUMERO ', '-'*10)
 print("Escolha o Nivel do jogo. \n 1 - Nivel 1 \n 2 - Nivel 2 \n 3 - Nivel 3")
 total_de_tentativas = int(input("Digite o nivel de dificuldade:\n"))
 
 if (total_de_tentativas == 1):
     total_de_tentativas = 10
+    pontuacao = 100
 elif (total_de_tentativas == 2):
     total_de_tentativas = 5
+    pontuacao = 50
 else:
     total_de_tentativas = 3
+    pontuacao = 30
     
-numero_secreto = 42
-pontuacao = 100
+numero_secreto = random.randint(1, 100)
+
 
 for tentativa in range(1, total_de_tentativas + 1):
     print('Tentativa {} de {}'.format(tentativa, total_de_tentativas))
@@ -29,5 +33,7 @@ for tentativa in range(1, total_de_tentativas + 1):
     else:
         print("Voce Errou! O Numero que você digitou é MENOR!")
         pontuacao -= 10
-    
+
+print("O Total de sua pontuação é ", + pontuacao)
+print(f"O numero secreto era: {numero_secreto}")
 print("FIM DE JOGO")
